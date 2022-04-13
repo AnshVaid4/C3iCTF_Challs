74fd543453d5afcdc00fb99bf2eaf52aa78e0e49
Ansh Vaid <anshvaid4@gmail.com>
2022-04-13 18:20:42 +0530

    done

--- AV_challs/jwt/jwt3/index.php
+++ AV_challs/jwt/jwt3/index.php
@@ -1,4 +1,4 @@
-<?php error_reporting(0); session_start(); ?>
+<?php error_reporting(0);?>
 <!DOCTYPE html>
 <head>
     <title>Login</title>
@@ -16,7 +16,31 @@
     <form method="POST" action="#">
         <label><center><i class="fas fa-globe"> Whois...</i></center></label><br>
         <center><input type="text" name="x" maxlength="100" placeholder="Enter domain name"></input></center><br>
-        <input type="hidden" name="token" maxlength="100" value="<?php if($_SESSION['x'] == null) {echo "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleGVjIjoiIn0.2HHKNG8ZZFb-NfQQfStMqKFLFM8CrbfizX7HMLB90lc";}else{ echo $_SESSION['x']; }?>" readonly required>
+        <input type="hidden" name="token" maxlength="100" value="<?php 
+    
+     // Create token header as a JSON string
+     $header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
+
+     // Create token payload as a JSON string
+     $payload = json_encode(['exec' =>  $_POST['x']]);
+
+     // Encode Header to Base64Url String
+     $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+
+     // Encode Payload to Base64Url String
+     $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+
+     // Create Signature Hash
+     $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, 'Sup3Rh4RdK3Y__xD', true);
+
+     // Encode Signature to Base64Url String
+     $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+
+     // Create JWT
+     $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+    echo $jwt;
+    
+    ?>" readonly required>
         <center><button name="submit">Go</button></center><br>
         <?php
             error_reporting(0);
