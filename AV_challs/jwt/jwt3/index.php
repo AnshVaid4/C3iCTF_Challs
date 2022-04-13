c35f7158ce0b5c30ae27deb37a914e78a6c9bae1
Ansh Vaid <anshvaid4@gmail.com>
2022-04-13 22:33:57 +0530

    done

--- AV_challs/jwt/jwt3/index.php
+++ AV_challs/jwt/jwt3/index.php
@@ -45,38 +45,23 @@
         <?php
             error_reporting(0);
             if(isset($_POST['submit']) and $_POST['x'] != null and $_POST['token'] != null)
-            {
-               
-
-                // Create token header as a JSON string
-                $header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
-
-                // Create token payload as a JSON string
-                $payload = json_encode(['exec' =>  $_POST['x']]);
-
-                // Encode Header to Base64Url String
-                $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
-
-                // Encode Payload to Base64Url String
-                $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
-
-                // Create Signature Hash
-                $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, 'Sup3Rh4RdK3Y__xD', true);
-
-                // Encode Signature to Base64Url String
-                $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
-
-                // Create JWT
-                $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
-                $token=($_POST['token']);
-                // echo $token;
+	    {
+		$substitutions = array(
+	        ';' => '',
+		'|' => '',
+		'||' => '',
+		'#' => '',
+		'\'' => '',
+	        '%27' => '',
+		'%23' => '',
+		    ); 
+		$jwt = str_replace( array_keys( $substitutions ), $substitutions, $jwt ); 
+                $token=($jwt);
                 $arr=explode(".",$token);
                 $data=base64_decode($arr[1]);
-                $id=json_decode($data,true)['exec'];
-                echo $id;
-                     
+                $command=json_decode($data,true)['exec'];
                 $_SESSION['x'] = $jwt; 
-                passthru();
+                passthru('whois '.$command);
             }
             if (isset($_POST['submit']) and ($_POST['x'] == null))
             {
